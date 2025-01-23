58f2dbdadcfd5d9c61d04cfb9473b39372ec872f
root <root@aopl.miescor.ph>
2025-01-23 09:46:14 +0800

    update blabla

--- MIESCOR-AOPL/app/Filament/Pages/TicketsKanbanBoard.php
+++ MIESCOR-AOPL/app/Filament/Pages/TicketsKanbanBoard.php
@@ -12,6 +12,8 @@ class TicketsKanbanBoard extends KanbanBoard
     protected static string $model = Ticket::class;
     protected static string $statusEnum = TicketStatus::class;
     protected static string $recordTitleAttribute = 'title';
+    protected static ?string $title = 'Tickets';
+    protected static ?string $navigationIcon = 'heroicon-o-ticket';
 
     protected function statuses(): Collection
     {
@@ -19,14 +21,20 @@ class TicketsKanbanBoard extends KanbanBoard
     }
     protected function records(): Collection
     {
-        return Ticket::latest('updated_at')->get();
+        //return Ticket::latest('updated_at')->get();
+        return Ticket::ordered()->get();
     }
     public function onStatusChanged(int $recordId, string $status, array $fromOrderedIds, array $toOrderedIds): void
     {
         Ticket::find($recordId)->update(['status' => $status]);
+        Ticket::ignoreTimestamps();
+        Ticket::setNewOrder($toOrderedIds);
+        Ticket::ignoreTimestamps(false);
     }
     public function onSortChanged(int $recordId, string $status, array $orderedIds): void
     {
-        
+        Ticket::ignoreTimestamps();
+        Ticket::setNewOrder($orderedIds);
+        Ticket::ignoreTimestamps(false);
     }
 }
